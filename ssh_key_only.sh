b77218f2b07211eebc008b5e8ad0fdaeb25d873a
era <the@communist.dev>
2021-11-08 20:27:03 +0000

    adding script to send all the keys to the vps and to disable password authentication for ssh

---
+++ ssh_key_only.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+echo "Copying key to ${2} for user ${1}"
+ssh-copy-id "${1}@${2}"
+echo 'Done'
+echo '    '
+echo 'You may be prompt for your sudo password in worder to disable PasswordAuthentication'
+ssh "${1}@${2}" 'sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config'
+echo "Done"
+echo '    '
+echo "Restartign sshd"
+ssh "${1}@${2}" 'sudo service sshd restart'
+echo "Done"
+echo '    '
+ssh "${1}@${2}" 'uptime'
+echo "Congrats, you can ssh only with the keys setup for your user in this computer"
+echo '    '
+
